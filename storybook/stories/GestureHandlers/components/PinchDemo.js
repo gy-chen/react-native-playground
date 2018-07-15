84b2ca5b745440870346d835596819a26d81479d
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-07-15 14:10:32 +0800

    update

--- storybook/stories/GestureHandlers/components/PinchDemo.js
+++ storybook/stories/GestureHandlers/components/PinchDemo.js
@@ -3,12 +3,18 @@ import {Animated} from 'react-native';
 import styled from 'styled-components';
 import {PinchGestureHandler, State} from 'react-native-gesture-handler';
 
-const Container = Animated.createAnimatedComponent(styled.View`
+const Container = styled.View`
     flex: 1;
     justifyContent: center;
     alignItems: center;
     backgroundColor: #F5FCFF;
     zIndex: -1;
+`;
+
+const AnimatedContainer = Animated.createAnimatedComponent(styled.View`
+    flex: 1;
+    justifyContent: center;
+    alignItems: center;
 `);
 
 const StyledText = styled.Text`
@@ -25,17 +31,60 @@ export default class PinchDemo extends Component {
         this._baseScale = new Animated.Value(1);
         this._pinchScale = new Animated.Value(1);
         this._containerScale = Animated.multiply(this._baseScale, this._pinchScale);
+
+        this._baseCenterX = new Animated.Value(0);
+        this._baseCenterY = new Animated.Value(0);
+        this._lastFocalX = 0;
+        this._baseFocalX = new Animated.Value(0);
+        this._lastFocalY = 0;
+        this._baseFocalY = new Animated.Value(0);
+        this._lastTranslateX = 0;
+        this._baseTranslateX = new Animated.Value(0);
+        this._containerTranslateX = Animated.add(this._baseTranslateX, Animated.multiply(
+            Animated.add(new Animated.Value(1), Animated.multiply(-1, this._pinchScale)),
+            Animated.add(this._baseFocalX, Animated.multiply(-1, this._baseCenterX)))
+        );
+        this._lastTranslateY = 0;
+        this._baseTranslateY = new Animated.Value(0);
+        this._containerTranslateY = Animated.add(this._baseTranslateY, Animated.multiply(
+            Animated.add(new Animated.Value(1), Animated.multiply(-1, this._pinchScale)),
+            Animated.add(this._baseFocalY, Animated.multiply(-1, this._baseCenterY))
+        ));
         this._onPinchGestureEvent = Animated.event(
-            [{nativeEvent: {scale: this._pinchScale}}]
+            [{
+                nativeEvent: {
+                    scale: this._pinchScale
+                }
+            }]
         );
         this._onPinchHandlerStateChange = this._onPinchHandlerStateChange.bind(this);
+        this._onLayout = this._onLayout.bind(this);
+    }
+
+    _onLayout({nativeEvent: {layout: {x, y, width, height}}}) {
+        this._lastCenterX = width / 2.;
+        this._baseCenterX.setValue(this._lastCenterX);
+        this._lastCenterY = height / 2.;
+        this._baseCenterY.setValue(this._lastCenterY);
     }
 
     _onPinchHandlerStateChange = event => {
+        if (event.nativeEvent.oldState === State.BEGAN) {
+            this._lastFocalX = event.nativeEvent.focalX;
+            this._baseFocalX.setValue(this._lastFocalX);
+            this._lastFocalY = event.nativeEvent.focalY;
+            this._baseFocalY.setValue(this._lastFocalY);
+        }
+
         if (event.nativeEvent.oldState === State.ACTIVE) {
             this._lastScale *= event.nativeEvent.scale;
             this._baseScale.setValue(this._lastScale);
             this._pinchScale.setValue(1);
+
+            this._lastTranslateX += (1 - event.nativeEvent.scale) * (this._lastFocalX - this._lastCenterX);
+            this._baseTranslateX.setValue(this._lastTranslateX);
+            this._lastTranslateY += (1 - event.nativeEvent.scale) * (this._lastFocalY - this._lastCenterY);
+            this._baseTranslateY.setValue(this._lastTranslateY);
         }
     };
 
@@ -46,16 +95,21 @@ export default class PinchDemo extends Component {
                 onHandlerStateChange={this._onPinchHandlerStateChange}
             >
                 <Container
-                    style={{
-                        ...this.props.style,
-                        transform: [
-                            {scale: this._containerScale}
-                        ]
-                    }}
+                    onLayout={this._onLayout}
                 >
-                    <StyledText>
-                        Hello Pinch
-                    </StyledText>
+                    <AnimatedContainer
+                        style={{
+                            ...this.props.style,
+                            transform: [
+                                {scale: this._containerScale},
+                                {translateX: this._containerTranslateX},
+                                {translateY: this._containerTranslateY}
+                            ]
+                        }}>
+                        <StyledText>
+                            Hello Pinch
+                        </StyledText>
+                    </AnimatedContainer>
                 </Container>
             </PinchGestureHandler>
         )
